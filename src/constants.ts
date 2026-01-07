35e122e62b721478c5e22606327376d09c44658f
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-08 01:42:09 +0600

    feat: add project to constants

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import { Project } from "./types";
+
+export const PROJECTS: Project[] = [
+  {
+    id: "1",
+    title: "Furniro",
+    description:
+      "A full stack e-commerce web application built with modern web technologies.",
+    tags: ["Next.js", "Node.js", "Express", "MongoDB"],
+    image: "/Homepage.png",
+    link: "https://furniro-hazel-nine.vercel.app/",
+    github: "https://github.com/mdshakerullahS/Furniro",
+  },
+];
