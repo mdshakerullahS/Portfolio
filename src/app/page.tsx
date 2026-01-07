1e4fec12a8e88c9bdcf33bbececda4498b41b50d
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-08 01:40:48 +0600

    update: add projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <div className="cyber-grid">
       <Hero />
       <About />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,23 @@
+import { PROJECTS } from "@/constants";
+import ProjectCard from "./ProjectCard";
+import { FC } from "react";
+
+const Projects: FC = () => {
+  return (
+    <section id="projects" className="pt-24 relative">
+      <div className="container mx-auto px-6 space-y-12">
+        <h2 className="text-2xl md:text-5xl text-center font-black italic">
+          FEATURED <span className="text-blue-400">PROJECTS</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {PROJECTS.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
