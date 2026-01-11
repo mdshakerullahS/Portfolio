f82b017e3380cd832f4b64df47ac181c0a0378b6
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-12 05:02:41 +0600

    fix: fix smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
