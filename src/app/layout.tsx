c4a9b5c92857e964dee994899b922b2b13dc6186
Shakerullah <sourovmdshakerullah@gmail.com>
2026-04-03 01:26:24 +0600

    update: metadata and change og-image and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL!;
-const TITLE = "Shakerullah - Full-Stack Web Developer";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "https://shakerullah.vercel.app";
+const TITLE =
+  "Shakerullah - Full-Stack Developer | Building fast, Scalable Web apps that actually survive production.";
 const DESCRIPTION =
-  "I build end-to-end web applications with modern frontend technologies and scalable backend systems. Passionate about clean UI, performance, and real-world problem solving.";
+  "Full-stack developer focused on building fast, scalable web applications using modern technologies. I've built real projects with authentication, dashboards, and APIs.";
 const NAME = "Md Shakerullah Sourov";
 
 export const metadata: Metadata = {
@@ -48,7 +50,13 @@ export const metadata: Metadata = {
         url: "/og-image.png",
         width: 1200,
         height: 630,
-        alt: TITLE,
+        alt: "Shakerullah Portfolio Preview",
+      },
+      {
+        url: "/og-image-small.png",
+        width: 600,
+        height: 315,
+        alt: "Shakerullah Portfolio Preview",
       },
     ],
   },
@@ -56,6 +64,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     creator: "@mdshakerullahS",
+    site: "@mdshakerullahS",
     images: ["/og-image.png"],
     description: DESCRIPTION,
     title: TITLE,
