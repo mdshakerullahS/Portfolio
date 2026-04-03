fd753e7dd9608c8011179d1fb102ff0721fe5ebd
Shakerullah <sourovmdshakerullah@gmail.com>
2026-04-03 23:22:22 +0600

    chore: configure eslint rules & prettier and fix relevanc issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,66 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
+    "node_modules/**",
     ".next/**",
+    ".dist/**",
     "out/**",
     "build/**",
+    "public/**",
     "next-env.d.ts",
   ]),
+  {
+    files: ["**/*.{ts,js,tsx,jsx,mjs,cjs}"],
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "react/jsx-uses-vars": "error",
+      "no-undef": "error",
+      "no-unreachable": "error",
+      "no-constant-condition": "warn",
+      "no-debugger": "error",
+
+      eqeqeq: ["error", "always"],
+      "no-var": "error",
+      "prefer-const": "error",
+      "no-useless-return": "error",
+      "no-empty": ["error", { allowEmptyCatch: true }],
+      "no-shadow": "error",
+
+      "no-process-exit": "warn",
+      "no-sync": "warn",
+
+      semi: "off",
+      quotes: "off",
+      indent: "off",
+      "comma-dangle": "off",
+      "object-curly-spacing": "off",
+      "arrow-spacing": "off",
+      "prettier/prettier": "error",
+
+      "no-console": "warn",
+      "no-trailing-spaces": "error",
+      "eol-last": ["error", "always"],
+      "max-lines": ["warn", 300],
+      "max-depth": ["warn", 4],
+
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
